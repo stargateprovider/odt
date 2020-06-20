0e92579c7ae4f3aa5aaf5fcf8d4ffdcbd5560e5a
stargateprovider <kana46@windowslive.com>
2020-06-20 13:22:06 +0300

    Add upload test

--- onedrive.js
+++ onedrive.js
@@ -25,48 +25,41 @@ var odOptions = {
 	    }
 	}
 
-	success: 'oneDriveFilePickerSuccess',
-	cancel: 'oneDriveFilePickerCancel',
-	error: 'oneDriveFilePickerError'
+	success: function(files) {
+		console.log(files);
+		alert(files);
+		var file = new Blob([files], {type: 'application/json'});
+
+		let a = document.createElement('a');
+		a.href = window.URL.createObjectURL(file);
+		a.download = 'abc.txt';
+		a.click();
+		//window.webkitURL.createObjectURL(file);
+		// var odOptions = {
+		// 	clientId: "5024a142-154d-45c4-9ca4-2013bca8919a",
+		// 	action: "download",
+		// 	multiSelect: true,
+		// 	openInNewWindow: false,
+		// 	advanced: {
+		// 		redirectUri: "https://stargateprovider.github.io/customnewtab/odt.html",
+		// 		queryParameters: "select=id,name,size,file,folder,@microsoft.graph.downloadUrl",
+		// 		filter: "folder,.json",
+		// 		accessToken: files.accessToken
+		// 	},
+		// 	success: 'oneDriveFilePickerSuccess2',
+		// 	cancel: 'oneDriveFilePickerCancel',
+		// 	error: 'oneDriveFilePickerError'
+		// }
+		// OneDrive.open(odOptions);
+	},
+	cancel: function(e) {
+		console.log('OneDrive Launch Cancelled!');
+	},
+	error: function() {
+		console.log('OneDrive Launch Failed!');
+	}
 }
 
-
-var oneDriveFilePickerError = function() {
-	console.log('OneDrive Launch Failed!');
-}
-var oneDriveFilePickerSuccess2 = function(files) {
-	console.log(files);
-}
-var oneDriveFilePickerSuccess = function(files) {
-	console.log(files);
-	alert(files);
-	var file = new Blob([files], {type: 'application/json'});
-
-	let a = document.createElement('a');
-	a.href = window.URL.createObjectURL(file);
-	a.download = 'abc.txt';
-	a.click();
-	//window.webkitURL.createObjectURL(file);
-	// var odOptions = {
-	// 	clientId: "5024a142-154d-45c4-9ca4-2013bca8919a",
-	// 	action: "download",
-	// 	multiSelect: true,
-	// 	openInNewWindow: false,
-	// 	advanced: {
-	// 		redirectUri: "https://stargateprovider.github.io/customnewtab/odt.html",
-	// 		queryParameters: "select=id,name,size,file,folder,@microsoft.graph.downloadUrl",
-	// 		filter: "folder,.json",
-	// 		accessToken: files.accessToken
-	// 	},
-	// 	success: 'oneDriveFilePickerSuccess2',
-	// 	cancel: 'oneDriveFilePickerCancel',
-	// 	error: 'oneDriveFilePickerError'
-	// }
-	// OneDrive.open(odOptions);
-}
-var oneDriveFilePickerCancel = function(e) {
-	console.log('OneDrive Launch Cancelled!');
-}
 
 function launchOneDrivePicker(){
 
